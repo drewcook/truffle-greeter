c5af0dd251eb4e5c71cdd6c0ea72423de5744f43
Drew Cook <drewcook29@gmail.com>
2021-12-23 21:25:41 -0700

    chore: Clean up UI and deploy contracts

--- fundraiser/client/src/App.js
+++ fundraiser/client/src/App.js
@@ -1,73 +1,52 @@
-import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
-import getWeb3 from "./getWeb3";
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import FundraiserContract from './contracts/Fundraiser.json'
+import getWeb3 from './getWeb3'
 
-import "./App.css";
+const App = () => {
+	const [web3, setWeb3] = useState(null)
+	const [accounts, setAccounts] = useState(null)
+	const [contract, setContract] = useState(null)
 
-class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+	useEffect(async () => {
+		try {
+			// Get network provider and web3 instance.
+			const web3Instance = await getWeb3()
 
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+			// Use web3 to get the user's accounts.
+			const web3Accounts = await web3.eth.getAccounts()
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+			// Get the contract instance.
+			const networkId = await web3.eth.net.getId()
+			const deployedNetwork = FundraiserContract.networks[networkId]
+			const contractInstance = new web3.eth.Contract(
+				FundraiserContract.abi,
+				deployedNetwork && deployedNetwork.address,
+			)
+			// Set web3, accounts, and contract to the state, and then proceed with an
+			// example of interacting with the contract's methods.
+			setWeb3(web3Instance)
+			setAccounts(web3Accounts)
+			setContract(contractInstance)
+		} catch (error) {
+			// Catch any errors for any of the above operations.
+			alert(`Failed to load web3, accounts, or contract. Check console for details.`)
+			console.error(error)
+		}
+	}, [])
 
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
+	console.log({ web3, accounts, contract })
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
-    }
-  };
+	if (!web3) {
+		return <div>Loading Web3, accounts, and contract...</div>
+	}
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
-
-  render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
-    return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
-    );
-  }
+	return (
+		<div className="App">
+			<h1>Fundraiser DApp</h1>
+			<p>Your Truffle Box is installed and ready.</p>
+		</div>
+	)
 }
 
-export default App;
+export default App
