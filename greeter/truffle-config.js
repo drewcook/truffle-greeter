36a4989f1b850183caf7a4362b4a85cdb5d09433
Drew Cook <drewcook29@gmail.com>
2021-12-23 20:14:49 -0700

    chore: Deployments and networks

--- greeter/truffle-config.js
+++ greeter/truffle-config.js
@@ -18,10 +18,8 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+require('dotenv').config()
+const HDWalletProvider = require('@truffle/hdwallet-provider')
 
 module.exports = {
 	contracts_build_directory: './client/src/contracts',
@@ -43,11 +41,11 @@ module.exports = {
 		// tab if you use this network and you must also set the `host`, `port` and `network_id`
 		// options below to some value.
 		//
-		// development: {
-		//  host: "127.0.0.1",     // Localhost (default: none)
-		//  port: 8545,            // Standard Ethereum port (default: none)
-		//  network_id: "*",       // Any network (default: none)
-		// },
+		development: {
+			host: '127.0.0.1', // Localhost (default: none)
+			port: 7545, // Standard Ethereum port (default: none)
+			network_id: 5777, // Any network (default: none)
+		},
 		// Another network with more advanced options...
 		// advanced: {
 		// port: 8777,             // Custom port
@@ -59,14 +57,25 @@ module.exports = {
 		// },
 		// Useful for deploying to a public network.
 		// NB: It's important to wrap the provider as a function.
-		// ropsten: {
-		// provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-		// network_id: 3,       // Ropsten's id
-		// gas: 5500000,        // Ropsten has a lower block limit than mainnet
-		// confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-		// timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-		// skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-		// },
+		// Our local Goerli node from OpenEthereum client
+		goerli: {
+			provider: () =>
+				new HDWalletProvider({
+					mnemonic: process.env.MNEMONIC, // random mnemonic?
+					providerOrUrl: 'http://127.0.0.1:8546',
+				}),
+			network_id: '*',
+		},
+
+		// Our Rinkeby ETH Wallet in MetaMask
+		rinkeby: {
+			provider: () =>
+				new HDWalletProvider({
+					mnemonic: process.env.MNEMONIC_2,
+					providerOrUrl: `https://rinkeby.infura.io/v3/${process.env.INFURA_PRODUCT_ID}`,
+				}),
+			network_id: '*',
+		},
 		// Useful for private networks
 		// private: {
 		// provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
