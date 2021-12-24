536bf5a992e5356a77739c25afdae167b3b8d5f5
Drew Cook <drewcook29@gmail.com>
2021-12-23 20:58:21 -0700

    chore: Add fundraiser base

---
+++ fundraiser/contracts/Fundraiser.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.21 <0.9.0;
+
+contract Fundraiser {
+    string public name;
+    string public url;
+    string public imageURL;
+    string public description;
+    address payable public beneficiary;
+    address public custodian;
+
+    constructor(
+        string memory _name,
+        string memory _url,
+        string memory _imageURL,
+        string memory _description,
+        address payable _beneficiary,
+        address _custodian
+    ) public {
+        name = _name;
+        url = _url;
+        imageURL = _imageURL;
+        description = _description;
+        beneficiary = _beneficiary;
+        custodian = _custodian;
+    }
+}
