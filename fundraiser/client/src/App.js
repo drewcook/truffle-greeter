ebc37619629b6723b52acbfc5bb5f2e7a7cd0a98
Drew Cook <drewcook29@gmail.com>
2021-12-26 12:17:34 -0700

    chore: Update client

--- fundraiser/client/src/App.js
+++ fundraiser/client/src/App.js
@@ -1,52 +1,82 @@
-import React, { useEffect, useState } from 'react'
-import './App.css'
-import FundraiserContract from './contracts/Fundraiser.json'
-import getWeb3 from './getWeb3'
+
+import React, { useState, useEffect } from "react";
+import FactoryContract from "./contracts/FundraiserFactory.json";
+import getWeb3 from "./utils/getWeb3";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+
+import NewFundraiser from './NewFundraiser'
+import Home from './Home'
+import Receipts from './Receipts'
+
+import "./App.css";
 
 const App = () => {
-	const [web3, setWeb3] = useState(null)
-	const [accounts, setAccounts] = useState(null)
-	const [contract, setContract] = useState(null)
+  const [state, setState] = useState({web3: null, accounts: null, contract: null});
 
-	useEffect(async () => {
-		try {
-			// Get network provider and web3 instance.
-			const web3Instance = await getWeb3()
+  useEffect(() => {
+    const init = async() => {
+      try {
+        // Get network provider and web3 instance.
+        const web3 = await getWeb3();
 
-			// Use web3 to get the user's accounts.
-			const web3Accounts = await web3.eth.getAccounts()
+        // Use web3 to get the user's accounts.
+        const accounts = await web3.eth.getAccounts();
 
-			// Get the contract instance.
-			const networkId = await web3.eth.net.getId()
-			const deployedNetwork = FundraiserContract.networks[networkId]
-			const contractInstance = new web3.eth.Contract(
-				FundraiserContract.abi,
-				deployedNetwork && deployedNetwork.address,
-			)
-			// Set web3, accounts, and contract to the state, and then proceed with an
-			// example of interacting with the contract's methods.
-			setWeb3(web3Instance)
-			setAccounts(web3Accounts)
-			setContract(contractInstance)
-		} catch (error) {
-			// Catch any errors for any of the above operations.
-			alert(`Failed to load web3, accounts, or contract. Check console for details.`)
-			console.error(error)
-		}
-	}, [])
+        // Get the contract instance.
+        const networkId = await web3.eth.net.getId();
+        const deployedNetwork = FactoryContract.networks[networkId];
+        const instance = new web3.eth.Contract(
+          FactoryContract.abi,
+          deployedNetwork && deployedNetwork.address,
+        );
 
-	console.log({ web3, accounts, contract })
+        // Set web3, accounts, and contract to the state, and then proceed with an
+        setState({web3, accounts, contract: instance});
 
-	if (!web3) {
-		return <div>Loading Web3, accounts, and contract...</div>
-	}
+      } catch(error) {
+        // Catch any errors for any of the above operations.
+        alert(
+          `Failed to load web3, accounts, or contract. Check console for details.`,
+        );
+        console.error(error);
+      }
+    }
+    init();
+  }, []);
 
-	return (
-		<div className="App">
-			<h1>Fundraiser DApp</h1>
-			<p>Your Truffle Box is installed and ready.</p>
-		</div>
-	)
+  const useStyles = makeStyles({
+    root: {
+      flexGrow: 1,
+    },
+  });
+
+  const runExample = async () => {
+    const { accounts, contract } = state;
+  };
+
+  return (
+    <div>
+      <Router>
+        <AppBar position="static" color="default" style={{ margin: 0 }}>
+          <Toolbar>
+           <Typography variant="h6" color="inherit">
+             <NavLink className="nav-link" to="/">Home</NavLink>
+           </Typography>
+           <NavLink className="nav-link" to="/new/">New</NavLink>
+          </Toolbar>
+       </AppBar>
+
+        <Route path="/" exact component={Home} />
+        <Route path="/new/" component={NewFundraiser} />
+        <Route path="/receipts" component={Receipts} />
+      </Router>
+    </div>
+  )
 }
 
-export default App
+
+export default App;
